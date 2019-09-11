9939a6f06abb84062cb1824c84f0dab48f52b4dc
bplindgren <bplindgren91@gmail.com>
2019-09-10 22:10:54 -0500

    tests fixed for heroku

--- src/test/java/com/GameTests/GameServiceTest.java
+++ src/test/java/com/GameTests/GameServiceTest.java
@@ -5,25 +5,17 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.json.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import com.entity.Game;
 import com.google.gson.Gson;
 import com.service.GameService;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class GameServiceTest {
 	
