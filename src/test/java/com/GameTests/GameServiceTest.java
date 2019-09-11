1efbee3f131fdb5330f4149206f52226fed61798
bplindgren <bplindgren91@gmail.com>
2019-09-10 22:12:10 -0500

    tests fixed for heroku

--- src/test/java/com/GameTests/GameServiceTest.java
+++ src/test/java/com/GameTests/GameServiceTest.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
